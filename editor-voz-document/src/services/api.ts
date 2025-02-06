f221a1d05e85f9bc97607615e4e153b6fa74d81f
Edinael Amaya <edinael.amaya@wiedii.com>
2025-02-06 15:07:47 -0500

    change in domain

--- editor-voz-document/src/services/api.ts
+++ editor-voz-document/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'http://3.128.255.243';
 
 // Configuración inicial de Axios
 const api = axios.create({
